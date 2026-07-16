0e174a632b3f21d75ec2cf1a71a3f4522e03e3cc
Lemphy <slusmisha16@gmail.com>
2026-07-16 16:20:03 +0300

    задание10

--- Декораторы и замыкания/10.py
+++ Декораторы и замыкания/10.py
@@ -3,22 +3,18 @@
 # добавлять его в список внутри замыкания и возвращать список,
 # содержащий не более 3-х последних добавленных сообщений (более старые должны удаляться).
 
-def history_buffer(total):
-    messages = [None] * total
-    index = 0
+def history_buffer(total: int):
+    messages = []
     def temp(message: str) -> list[str]:
-        nonlocal index
-        if index < total:
-            messages[index] = message
-            index += 1
+        if len(messages) >= total:
+            del messages[0]
+            messages.append(message)
             return messages
         else:
-            index = 0
-            messages[index] = message
-            index += 1
+            messages.append(message)
             return messages
     return temp
 
 func = history_buffer(3)
-for _ in range(10):
-    print(func(input('text: ')))
+for _ in range(8):
+    print(func(input('String: ')))
