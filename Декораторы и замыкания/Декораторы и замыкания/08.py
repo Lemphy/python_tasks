b583f5b661ee4298ca29b3e1460bc66d12f9b01e
Lemphy <slusmisha16@gmail.com>
2026-07-20 18:43:33 +0300

    задание 8

---
+++ Декораторы и замыкания/Декораторы и замыкания/08.py
@@ -0,0 +1,33 @@
+# Создай декоратор, который сохраняет результаты выполнения функции в словарь.
+# Если функция вызывается с теми же аргументами повторно, она должна возвращать готовый результат из кэша,
+# а не вычислять его заново.
+import time
+
+def cache_result(func):
+    storage = {5 : 25}
+    def wrapper(value):
+        if value not in storage:
+            print('Функция была вызвана!')
+            storage[value] = func(value)
+            return storage[value]
+        else:
+            print('Функция не была вызвана!')
+            return storage[value]
+    return wrapper
+
+def timer(func):
+    def wrapper(*args):
+        start = time.time()
+        work = func(*args)
+        end = time.time()
+        print(f'Время работы: {end - start:.2e}')
+        return work
+    return wrapper
+
+@timer
+@cache_result
+def to_square(value):
+    return value ** 2
+
+for i in range(3,6):
+    print(to_square(i))
